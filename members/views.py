807bfdf35a950a3d235106b2e31ae2b56d9b1f0b
Suleman <Suleman1214>
2026-06-08 17:25:17 +0500

    Add slug field

--- members/views.py
+++ members/views.py
@@ -12,14 +12,13 @@ def members(request):
   }
   return HttpResponse(template.render(context, request))
   
-def details(request, id):
-  mymember = Member.objects.get(id=id)
-  template = loader.get_template('details.html')
-  context = {
-    'mymember': mymember,
-  }
-  return HttpResponse(template.render(context, request))
-  
+def details(request, slug):
+    mymember = Member.objects.get(slug=slug)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
