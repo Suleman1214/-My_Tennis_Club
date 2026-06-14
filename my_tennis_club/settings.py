04650d2eb84e888b77f72c24aed59e15ef4d2cf1
Suleman <Suleman1214>
2026-06-14 23:05:21 +0500

    Add Bootstrap 5

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -82,12 +83,15 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 
 
+# my_tennis_club/settings.py
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 # ======
 # DATABASES = {
 #     'default': dj_database_url.config(
